1c63a946ba83961921b34e3de7dd837923493544
webdevKmer <webdevKmer@gmail.com>
2024-02-03 11:23:29 +0000

    update main app

---
+++ main_app/urls.py
@@ -0,0 +1,15 @@
+"""
+URL configuration for main_app project.
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  
+    path('blog/', include('blog.urls')),
+]
